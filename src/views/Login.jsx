ac6f82e36b4e2f501509aabfb53024cd4679304f
Tarasato <taramiratsu.xato@gmail.com>
2025-04-02 16:25:40 +0700

    ok

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -13,7 +13,7 @@ import axios from "axios";
 const API_URL = import.meta.env.API_URL;
 
 export const Login = () => {
-  const navigator = useNavigate();
+  const navigator = useNavigate()
 
   const [travellerEmail, setTravellerEmail] = useState('');
   const [travellerPassword, setTravellerPassword] = useState('');
@@ -21,102 +21,76 @@ export const Login = () => {
   const handleLoginClick = async (e) => {
     e.preventDefault();
 
-    //validate UI
-    if(travellerEmail.length == 0){
-      alert('กรุณาป้อนชื่อผู้ใช้(อีเมล)')
-      return
-    }else if (travellerPassword.length == 0){
-      alert('กรุณาป้อนรหัสผ่าน')
-      return
+    //validate ui
+    if (travellerEmail.length == 0) {
+      alert('ป้อนชื่อผู้ใช้ (อีเมล์) ด้วย !!!')
+      return;
+    } else if (travellerPassword.length == 0) {
+      alert('ป้อนรหัสผ่าน ด้วย !!!')
+      return;
     }
 
-    // ส่งข้อมูลไป API เพื่อตรวจสอบและไปหน้าถัดไป MyTravel (/mytravel)
+    //ส่งข้อมูลไปยัง API เพื่อตรวจสอบและไปยังหน้าถัดไป MyTravel (/mytravel)
     try {
-      // const response = await fetch(`http://localhost:4000/traveller/${travellerEmail}/${travellerPassword}`, { //'http://localhost:4000/traveller/+travellerEmail+'/'+travellerPassword'
-      //   method: 'GET',
-      // })
+      // const response = await fetch('http://localhost:4000/traveller/'+travellerEmail+'/'+travellerPassword,{
       
-      const response = await axios.get(API_URL+`/traveller/${travellerEmail}/${travellerPassword}`)
+      // const response = await fetch(`http://localhost:4000/traveller/${travellerEmail}/${travellerPassword}`,{
+      //   method: 'GET'
+      // })
 
-      if(response.status == 200){
-        //เอาข้อมูลของ Teaveller ไปเก็บไว้ใน memory
+      const response = await axios.get(`${API_URL}/traveller/${travellerEmail}/${travellerPassword}`)
+
+      if (response.status == 200) {
+        // window.location.href('/mytravel')
+        //เอาข้อมูลของ Traveller ที่ Login ผ่านเก็บใส่ memory
+
         // const data = await response.json()
         // localStorage.setItem('traveller', JSON.stringify(data["data"]))
 
         localStorage.setItem('traveller', JSON.stringify(response.data["data"]))
-
-        //เปลี่ยนหน้าไปที่ /mytravel
+        
+        //แล้วค่อยเปิดไปหน้า /mytravel
         navigator('/mytravel')
-        }else if(response.status == 404){
-          alert('ชื่อผู้ใช้(Email)หรือรหัสผ่านไม่ถูกต้อง')
-        }else{
-          alert('เข้าสู่ระบบไม่สำเร็จ กรุณาลองใหม่อีกครั้ง' + response.status)
-            }
+      } else if (response.status == 404) {
+        alert('ชื่อผู้ใช้รหัสผ่าน ไม่ถูกต้อง')
+      } else {
+        alert('Login ไม่สำเร็จ กรุณาลองใหม่อีกครั้ง')
+      }
     } catch (error) {
-      alert('เกิดข้อผิดพลาด: ' + error)
+      alert('พบข้อผิดพลาดในการทำงาน: ', error)
     }
-
   }
 
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box sx={{ width: "40%", boxShadow: 4, mx: "auto", p: 5 }}>
-        <Typography
-          variant="h3"
-          sx={{ textAlign: "center", fontWeight: "bold" }}
-        >
+    <Box sx={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'center' }} >
+      <Box sx={{ width: '40%', boxShadow: 4, mx: 'auto', p: 5 }}>
+        <Typography variant='h3' sx={{ textAlign: 'center', fontWeight: 'bold' }}>
           Travel DTI
         </Typography>
-        <Avatar
-          src={travel}
-          alt="travel logo"
-          sx={{ width: 150, height: 150, mx: "auto", my: 2 }}
-        />
-        <Typography
-          variant="h5"
-          sx={{ textAlign: "center", fontWeight: "bold" }}
-        >
+        <Avatar src={travel} alt="travel logo"
+          sx={{ width: 150, height: 150, mx: 'auto', my: 2 }} />
+        <Typography variant='h5' sx={{ textAlign: 'center', fontWeight: 'bold' }}>
           เข้าใช้งานระบบ
         </Typography>
-        <Typography sx={{ fontWeight: "bold", mt: 2, mb: 1 }}>
-          ชื่อผู้ใช้ (Email)
+        <Typography sx={{ fontWeight: 'bold', mt: 4, mb: 1 }} >
+          ชื่อผู้ใช้ (E-Mail)
         </Typography>
-        <TextField fullWidth value={travellerEmail} onChange={(e) => setTravellerEmail(e.target.value)}/>
-        <Typography sx={{ fontWeight: "bold", mt: 2, mb: 1 }}>
+        <TextField fullWidth value={travellerEmail} onChange={(e) => setTravellerEmail(e.target.value)} />
+        <Typography sx={{ fontWeight: 'bold', mt: 2, mb: 1 }} >
           รหัสผ่าน
         </Typography>
-        <TextField fullWidth type="password" value={travellerPassword} onChange={(e) => setTravellerPassword(e.target.value)}/>
-        <Button
-          variant="contained"
-          fullWidth
-          onClick={handleLoginClick}
-          sx={{ mt: 2, py: 2, backgroundColor: "#259e69" }}
-        >
-          Login
+        <TextField fullWidth type='password' value={travellerPassword} onChange={(e) => setTravellerPassword(e.target.value)} />
+        <Button variant="contained" fullWidth onClick={handleLoginClick}
+          sx={{ mt: 4, py: 2, backgroundColor: '#259e69' }} >
+          LOGIN
         </Button>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            mt: 2,
-            mb: 1,
-          }}
-        >
-          <Typography sx={{ mr: 1 }}>ยังไม่มีบัญชีผู้ใช้?</Typography>
-          <Link to="/register" style={{ textDecoration: "none", color: "#259e69" }}>
-            <Typography sx={{ fontWeight: "bold" }}>ลงทะเบียน</Typography>
-          </Link>
-        </Box>
+
+        <Link to='/register' style={{ textDecoration: 'none', color: '#259e69' }}>
+          <Typography sx={{ fontWeight: 'bold', mt: 2, mb: 1, textAlign: 'center' }} >
+            ลงทะเบียน
+          </Typography>
+        </Link>
       </Box>
     </Box>
-  );
+  )
 };
